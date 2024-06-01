610bd4b759ff6f7bfc44964c0e40a9e3aac49732
FLOVVI <flovvi78@gmail.com>
2024-06-01 14:24:09 +0300

    Add

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+from telebot import types
+
+from config import *
+
+print("Active")
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=["start"])
+def start(message): ...
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call): ...
+
+
+@bot.message_handler(content_types=["text"])
+def handler_text(message): ...
+
+
+bot.polling(none_stop=True, interval=0, timeout=20)
